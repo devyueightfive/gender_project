da389f071de5b3ae28f1238926f007b186bbbb8d
devyueightfive <dev.yu85@yandex.ru>
2020-08-13 11:57:17 +0300

    bat with commands

---
+++ create_project.sh
@@ -0,0 +1,4 @@
+conda install -c conda-forge pyscaffold -y
+pip install pyscaffoldext-dsproject -q
+putup --dsproject --no-skeleton gender_project
+sudo apt-get install python3-dev default-libmysqlclient-dev build-essential
